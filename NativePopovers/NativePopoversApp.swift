23edd06deebf58d22e782c0def2cb1639a5bc60a
Berkay-Disli <berkaydisli.work@gmail.com>
2023-01-22 15:31:17 +0300

    Initial Commit

---
+++ NativePopovers/NativePopoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativePopoversApp.swift
+//  NativePopovers
+//
+//  Created by Berkay Disli on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct NativePopoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
